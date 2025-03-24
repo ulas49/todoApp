cac44639f5862e75c498000d4883c72d882afac0
Ulas <ulasbyz4949@gmail.com>
2025-03-24 23:43:55 +0300

    Implement TodoCarts

--- client/src/components/TodoCart.tsx
+++ client/src/components/TodoCart.tsx
@@ -6,9 +6,9 @@ const mockdata = {
   image:
     'https://images.unsplash.com/photo-1437719417032-8595fd9e9dc6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=600&q=80',
   title: 'Verudela Beach',
-  country: 'Croatia',
+
   description:
-    'Completely renovated for the season 2020, Arena Verudela Bech Apartments are fully equipped and modernly furnished 4-star self-service apartments located on the Adriatic coastline by one of the most beautiful beaches in Pula.',
+    'Read a book',
   badges: [
     { emoji: '☀️', label: 'Sunny weather' },
     { emoji: '🦓', label: 'Onsite zoo' },
@@ -18,8 +18,8 @@ const mockdata = {
   ],
 };
 
-export function BadgeCard() {
-  const { image, title, description, country, badges } = mockdata;
+export function TodoCart() {
+  const { image, title, description, badges } = mockdata;
   const features = badges.map((badge) => (
     <Badge variant="light" key={badge.label} leftSection={badge.emoji}>
       {badge.label}
@@ -27,9 +27,9 @@ export function BadgeCard() {
   ));
 
   return (
-    <Card withBorder radius="md" p="md" className={classes.card}>
+    <Card withBorder radius="md" p="md" className='w-[300px]    max-h-[500px]   '>
       <Card.Section>
-        <Image src={image} alt={title} height={180} />
+        <Image src={image} alt={title} className='h-[200px]' />
       </Card.Section>
 
       <Card.Section className={classes.section} mt="md">
@@ -37,9 +37,6 @@ export function BadgeCard() {
           <Text fz="lg" fw={500}>
             {title}
           </Text>
-          <Badge size="sm" variant="light">
-            {country}
-          </Badge>
         </Group>
         <Text fz="sm" mt="xs">
           {description}
@@ -47,9 +44,6 @@ export function BadgeCard() {
       </Card.Section>
 
       <Card.Section className={classes.section}>
-        <Text mt="md" className={classes.label} c="dimmed">
-          Perfect for you, if you enjoy
-        </Text>
         <Group gap={7} mt={5}>
           {features}
         </Group>
--- client/src/pages/Todos.tsx
+++ client/src/pages/Todos.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import { HeaderMegaMenu } from '../components/Header'
-
+import { TodoCart } from '../components/TodoCart'
 const Todos:React.FC = () => {
   return (
-    <div><HeaderMegaMenu/></div>
+    <div>
+        <HeaderMegaMenu/>
+        <div className='  w-full  '>
+        <div className=' grid grid-cols-4 gap-8  card-mb '> <TodoCart/>  <TodoCart/>  <TodoCart/> <TodoCart/> <TodoCart/> <TodoCart/> <TodoCart/> <TodoCart/></div>
+
+        </div>
+       
+        
+    </div>
   )
 }
 
